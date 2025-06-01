bb71cc731229da4040d8bf92c6aa042edae7085d
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-01 01:18:48 +0100

    update weather variable

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-if weather == "sunny":
+if weather=="sunny":
   message = "Wear a t-shirt and sunglasses."
-elif weather == "rainy":
+elif weather=="rainy":
   message = "Don't forget your umbrella and a raincoat."
-elif weather == "cold":
+elif weather=="cold":
   message = "Make sure to wear a warm coat and a scarf."
 else:
   message = "Sorry, I don't have recommendations for this weather."
