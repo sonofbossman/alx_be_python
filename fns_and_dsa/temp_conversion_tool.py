f45eda68db4bfd0fe42ba8bb7d60e962afa68710
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-08 16:43:52 +0100

    A Python script named temp_conversion_tool.py. This script defines functions to convert temperatures between Celsius and Fahrenheit

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+  return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def main():
+  temperature = float(input("Enter the temperature to convert: "))
+  temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")[0]
+  if temperature_unit == 'F':
+    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+  elif temperature_unit == 'C':
+    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+  else:
+    print("Invalid conversion unit!")
+
+if __name__ == "__main__":
+  main()
