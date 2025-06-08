edf108e3f378c5b40934c07418ffd73fd7c3e8d3
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-08 17:07:44 +0100

    fix: handle exception causing script to fail

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,14 +8,17 @@ def convert_to_fahrenheit(celsius):
   return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
-  temperature = float(input("Enter the temperature to convert: "))
-  temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")[0]
-  if temperature_unit == 'F':
-    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
-  elif temperature_unit == 'C':
-    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
-  else:
-    print("Invalid conversion unit!")
+  try:
+    temperature = float(input("Enter the temperature to convert: "))
+    temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")[0]
+    if temperature_unit == 'F':
+      print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+    elif temperature_unit == 'C':
+      print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+    else:
+      print("Invalid conversion unit!")
+  except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
   main()
