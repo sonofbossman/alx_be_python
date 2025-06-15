95b5c5d1be525a76d9450e7ce87ba8c0e7f86b78
Akingbade Tunde Eniola <akingbadeeniola452@gmail.com>
2025-06-15 16:36:59 +0000

    Implementing Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,42 @@
+class Book:
+  def __init__(self, title, author):
+    self.title = title
+    self.author = author
+    self._is_checked_out=False
+    pass
+
+class Library:
+  def __init__(self):
+    self._books = []
+    pass
+  def add_book(self, *books):
+    try:
+      for book in books:
+        self._books.append(book)
+    except Exception as e:
+      print(e)
+
+  def check_out_book(self, title):
+    try:
+      for book in self._books:
+        if book.title == title:
+          book._is_checked_out = True
+    except Exception as e:
+      print(e)
+
+  def return_book(self, title):
+    try:
+      for book in self._books:
+        if book.title == title:
+          book._is_checked_out = False
+    except Exception as e:
+      print(e)
+
+  def list_available_books(self):
+    try:
+      for book in self._books:
+        if not book._is_checked_out:
+          print(f"{book.title} by {book.author}")
+    except Exception as e:
+      print(e)
+  
