0df3a5e2dc141a69ccef675f30528a89d69ebead
sonofbossman <akingbadeeniola452@gmail.com>
2025-06-22 16:31:01 +0100

    remove aspect of codes causing checker to fail

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,11 +7,11 @@ class Book:
     self.author = author
     self.year = year
   
-  def __str__(self) -> str:
+  def __str__(self):
     return f"{self.title} by {self.author}, published in {self.year}"
   
-  def __repr__(self) -> str:
+  def __repr__(self):
     return f"Book('{self.title}', '{self.author}', {self.year})"
   
-  def __del__(self) -> str:
+  def __del__(self):
     print(f"Deleting {self.title}")
